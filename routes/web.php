8596da14bd99286dab0a39fab40833ec9f55a72b
Akram Amkeched Ouriaghli <akram.amk00@gmail.com>
2026-02-14 15:02:07 +0100

    crear les rutes

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,11 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
+use App\Http\Controllers\GameController;
+
+Route::get('/', [GameController::class, 'index'])->name('games.index');
+Route::get('/games/create', [GameController::class, 'create'])->name('games.create');
+Route::post('/games', [GameController::class, 'store'])->name('games.store');
+Route::get('/games/{game}', [GameController::class, 'show'])->name('games.show');
+Route::patch('/games/{game}', [GameController::class, 'update'])->name('games.update');
